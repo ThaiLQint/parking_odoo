7dfa4afb01c12d757f9c8a46949bdb52041109bf
unknown <yesthanhnhan16@gmail.com>
2024-05-24 14:23:09 +0700

    merge

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -2,10 +2,7 @@
 from . import controllers_register_card_tag
 from . import controllers_product
 from . import controllers_histoty_lpr
-<<<<<<< HEAD
 
 from . import check_card_controller
 from . import webhook
 from . import history
-=======
->>>>>>> 28d7efaacc4993e575e361c03fefee412a40d6fd
