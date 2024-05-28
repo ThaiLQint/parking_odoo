38edd31c728c6b102b11459f9e80ee3de3c97c21
HieuDTt4Tek <157444035+HieuDTt4Tek@users.noreply.github.com>
2024-05-28 14:17:04 +0700

    Push lai

--- __manifest__.py
+++ __manifest__.py
@@ -22,7 +22,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['stock', 'product', 'base', 'web'],
+    'depends': ['contacts','stock', 'product', 'base', 'web'],
     "application": True,
     # always loaded
     'data': [
